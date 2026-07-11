5942dc1bbc181094e98b2db030b0c4167bd24eec
ramcharan-devupalli <ramcharan.devupalli9@gmail.com>
2026-07-11 22:29:16 +0530

    Restore Nodemailer for Railway deployment

--- backend/services/emailService.js
+++ backend/services/emailService.js
@@ -2,9 +2,14 @@ const nodemailer = require('nodemailer');
 const mailConfig = require('../config/mailConfig');
 
 module.exports = nodemailer.createTransport({
-  service: 'gmail',
+  host: 'smtp.gmail.com',
+  port: 465,
+  secure: true,
   auth: {
     user: mailConfig.gmailUser,
     pass: mailConfig.gmailPass
-  }
+  },
+  connectionTimeout: 30000,
+  greetingTimeout: 30000,
+  socketTimeout: 60000
 });
